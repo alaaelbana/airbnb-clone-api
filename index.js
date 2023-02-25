1fe8ca392e8aa5ec26e10834e5f81344da9e21ad
3laa elbana <77382094+alaaelbana@users.noreply.github.com>
2023-02-25 08:54:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,9 @@ const jwtSecret = "asdjdqwpoklc";
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173","https://airbnb-clone-nhqe.onrender.com"],
+    origin: "http://localhost:5173",
   })
 );
-
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL);
 
@@ -64,7 +63,12 @@ app.post("/login", async (req, res) => {
         {},
         (err, token) => {
           if (err) throw err;
-          res.cookie("token", token).json(user);
+          res
+            .cookie("token", token, {
+              sameSite: true,
+              secure: true,
+            })
+            .json(user);
         }
       );
     } else {
